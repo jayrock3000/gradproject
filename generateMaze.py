e175e5e9309757267237f86c562deca7544b865e
jayrock3000 <jeffrhoten@gmail.com>
2025-07-23 12:37:40 -0700

    Worked on maze generation

--- generateMaze.py
+++ generateMaze.py
@@ -1,3 +1,28 @@
-# Not sure what to put here
-# some form of maze generator
+'''
+Not sure what to put here
+some form of maze generator
 
+Let's review our options for maze generation, and the general steps needed to create one
+
+The two algorithms you're looking at for maze generation:
+1. Recursive Backtracking
+2. Prim's Algorithm
+
+
+'''
+
+maze = [
+    [1,9,5,"butts",1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    [1,1,1,1,1,1,1,1], 
+    ]
+
+print(f"The result is: {maze[0][0]}")
+print(f"The result is: {maze[0][1]}")
+print(f"The result is: {maze[0][2]}")
+print(f"The result is: {maze[0][3]}")
